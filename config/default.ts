f7ccda097eeabf2b63ef2407e10416958e9506a7
André Lima <a.andrelima@outlook.com.br>
2025-03-18 22:05:03 -0300

    :wrench: Integrando e configurando o Express

---
+++ config/default.ts
@@ -0,0 +1,4 @@
+export default {
+    // Exportando meu objeto com meus dados de configuração
+    port: 3003,
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
-console.log("Deu bom!!")
+// Arquivo principal da aplicação:
+import express from "express";
+import config from "config";
+
+const app = express();
+
+// JSON middleware para a rota - E para pode trafegar informações em JSON
+app.use(express.json());
+
+// app port 
+const port = config.get<number>("port");
+
+// Incializando a aplicação 
+// Contem alguns dados de configuração da aplicação 
+app.listen(port, async () => {
+    // Depois vou ter a conexão com o Banco de dados e isso pode demorar alguma tempo.
+    console.log(`Aplicação está funcionando na porta: ${port}`);
+});
+
+
+
