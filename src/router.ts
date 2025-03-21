c5001c5c4db49f781b5c8042e6569f3963d83dc0
André Lima <a.andrelima@outlook.com.br>
2025-03-20 21:35:55 -0300

    :construction: Criando uma Rota de teste da API

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
-// O arquivo que vai conter somente as Rotas
+// Arquivo que vai conter somente as Rotas
 
+// Importando funcionalidade Router ( para criar as rotas ) do express, e o Request, Response para trabalhar com as funções que recebem a Requisição e a Resposta.  
 import { Router, Request, Response } from "express";
 
 const router = Router();
