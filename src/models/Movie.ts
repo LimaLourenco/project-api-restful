b60c5b102f2f33ff295081733bca35cc1fe6ee89
André Lima <a.andrelima@outlook.com.br>
2025-04-14 21:36:09 -0300

    :sparkles: Criando o Model

---
+++ src/models/Movie.ts
@@ -0,0 +1,29 @@
+// Vou ter uma serie de campos para inserir
+
+// Sempre que eu cria uma entidade de model, preciso estabelecer os dois: model e Schema.
+// Em primeiro vou fazer o Schema.
+// Em segundo vou cria uma entidade de model, para realiza todos metodos com base no CRUD, que o banco precisa realizar.
+import { model, Schema} from "mongoose";
+
+// Schema/Esquema
+const movieSchema = new Schema(
+    {
+        // Todas as propridades que eu quero inserir e o tipo definido
+        title: { type: String },
+        rating: { type: Number },
+        description: { type: String },
+        director: { type: String },
+        stars: { type: Array },
+        poster: { type: String }
+    },
+    {
+        timestamps: true
+    }
+);
+
+// Com o Schema pronto, eu vou exportar o model deste Schema.
+// Nomeando o model e passando o Schema.
+// Depois quando eu trazer este MovieModel no meus controllers para poder ativar os metodos de criação e leitura, 
+// Eu vou ter acesso a todos os metodos que tem no mongoose, e vou conseguir criar e ler as minhas entidades de maneira bem facil. 
+export const MovieModel = model("Movies", movieSchema); 
+
