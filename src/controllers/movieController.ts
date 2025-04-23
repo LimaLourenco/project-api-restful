2e3316824a3b85e06469f429909b6949ee173696
André Lima <a.andrelima@outlook.com.br>
2025-04-22 21:59:43 -0300

    :sparkles: Criando o Controller

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -1,1 +1,16 @@
 // Linkando as Rotas até meu controller
+
+// Trabalhando na logica
+import { Request, Response } from "express";
+
+// Model - Porque terei a sessão para trabalhar com Banco de dados, com as operações possiveis que irei aplicar.
+import { MovieModel } from "../models/Movies";
+
+// Logger - Para trabalhar com as mensagens de erro e de info ( informação )
+import Logger from "../../config/logger";
+
+// Criando as minhas funções de criação, leitura e etc...
+// Todas funções serão assincronas, porque irei trabalhar com o Banco de dados, e para também poder esperar o Banco de dados responder com os dados.
+export async function createMovie( req: Request, res: Response ) {
+    return res.status(200).send("Deu certo o controller");
+}
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@
 
 // Importando funcionalidade Router do express ( Para criar as rotas ), e o Request e Response para trabalha com as funções que receberam as Requisições e as Respostas.  
 import { Router, Request, Response } from "express";
+import { createMovie } from "./controllers/movieController";
 
 const router = Router();
 
@@ -12,4 +13,13 @@ const router = Router();
 export default router.get("/test", (req: Request, res: Response) => {
     // Responde com o status 200 e com a mensagem de "Trabalhando na API"
     res.status(200).send("Trabalhando na API");
-});
+   
+})
+.post("/movie", async (req: Request, res: Response) => {
+    try {
+        await createMovie(req, res);
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+}); // Cada Rota aqui, vai corresponder a uma função do meu Controller.
+
