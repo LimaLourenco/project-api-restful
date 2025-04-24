d4bf8154ed5b115b99a248d7af4173ffe971fb4e
André Lima <a.andrelima@outlook.com.br>
2025-04-23 23:16:46 -0300

    :sparkles: Inserindo dados

--- src/controllers/movieController.ts
+++ src/controllers/movieController.ts
@@ -12,5 +12,22 @@ import Logger from "../../config/logger";
 // Criando as minhas funções de criação, leitura e etc...
 // Todas funções serão assincronas, porque irei trabalhar com o Banco de dados, e para também poder esperar o Banco de dados responder com os dados.
 export async function createMovie( req: Request, res: Response ) {
-    return res.status(200).send("Deu certo o controller");
+    // return res.status(200).send("Deu certo o controller");
+
+    try {
+       // Primeiro recebo os dados pelo req.body, ou seja pelo corpo da requisição, e os dados vindo em formato json. 
+       const data = req.body;
+       
+       // Criando novo movie
+       // Esperando o MovieModel dar a entrada para ativar o metodo create para criar o dado que foi passado/fornecido.
+       const movie = await MovieModel.create(data);
+       
+       // Criado o movie, eu mando de volta como resposta em formato json. 
+       // E para ser utilizado, por exemplo - para exibir no front-end com alguma informação a mais que seja útil.
+       return res.status(201).json(movie); 
+    } catch (error: any) {
+        // Obs 1: As vezes error.message não entende que vai vir uma propriedade de message de erro.
+        // Obs 2: Colocando o erro: any, vou ter mais controle, e vai vir um objeto de erro com variados tipos. 
+        Logger.error(`Erro no sistema: ${error.message}`); 
+    }
 }
