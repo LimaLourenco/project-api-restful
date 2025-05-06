163997cb5820830c0c992678f07b0fd5f52cff82
André Lima <a.andrelima@outlook.com.br>
2025-05-05 21:45:31 -0300

    :sparkles: Criando Validação de filmes

--- src/middleware/movieValidation.ts
+++ src/middleware/movieValidation.ts
@@ -1,12 +1,17 @@
 // Criando o Middleware para a validação de filmes
 
-// Importando o body que é o corpo da requisição 
+// Importando o body que é o corpo da requisição pelo express-validator
 import { body } from "express-validator";
 
 export const movieCreateValidation = () => {
     return [
         body("title")
             .isString()
-            .withMessage("O título é obrigatório"),
+            .withMessage("O título é obrigatório")
+            .isLength({min: 5})
+            .withMessage("O titulo precisa ter minimo 5 caracteres"),
+        body("rating")
+            .isNumeric()
+            .withMessage("A nota deve ser um número")
     ];
 }
